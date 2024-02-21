60117d8db77aac9b262b7785194913c2cbc5c298
Neaz Mahmood <neazn694@gmail.com>
2024-02-21 21:37:03 +0600

    initail setup done, working on nav component

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
 
 const Nav = () => {
-  return <div>Nav</div>
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">test@gmail.com</p>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
